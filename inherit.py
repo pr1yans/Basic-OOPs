0e1bb179f107d81bd87094ca083e232031111545
Priyansh <bpriyansh229@gmail.com>
2025-09-30 12:28:20 +0530

    inheritence

---
+++ inherit.py
@@ -0,0 +1,24 @@
+class Animal:
+    def __init__(self, Name):
+        self.Name = Name
+        
+    def rough():
+        print('he is calling')
+        
+    def speak(self):
+        print(f"animal makes sound {self.Name}")
+        
+        
+class Dog(Animal):
+    def __init__(self, Name):
+        super().__init__(Name)
+    def speak(self):
+        super().speak()
+        print(f"{self.Name} barks")
+        
+        
+a  = Animal('billy')
+a.speak()
+
+d = Dog("tommy")
+d.speak()
